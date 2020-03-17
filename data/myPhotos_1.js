b08e5bea1587d24b9011647073e799f2520bd122
jcadag <62292076+jcadag@users.noreply.github.com>
2020-03-17 21:06:26 +0800

    Add files via upload

---
+++ data/myPhotos_1.js
@@ -0,0 +1,10 @@
+var json_myPhotos_1 = {
+"type": "FeatureCollection",
+"name": "myPhotos_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": "f1c3cc0e-f961-45d4-b546-a4cff0881931", "Name": "20200119_081552.jpg", "Date": "2020-01-19", "Time": "08:15:53.000", "Lon": 121.0696618, "Lat": 14.653796299722222, "Altitude": null, "North": null, "Azimuth": null, "Camera Mak": "samsung", "Camera Mod": "SM-G975F", "Title": null, "Comment": null, "Path": "C:\/Users\/Jake Rom Cadag\/Documents\/Academic\/GIS\/Mastery\/Import_Photos\\20200119_081552.jpg", "RelPath": ".\/Import_Photos\/20200119_081552.jpg", "Timestamp": "2020\/01\/19 08:15:53.000" }, "geometry": { "type": "Point", "coordinates": [ 121.0696618, 14.653796299722222 ] } },
+{ "type": "Feature", "properties": { "ID": "636ba353-8c8c-490a-a13b-f501b8bc4649", "Name": "20200301_143657.jpg", "Date": "2020-03-01", "Time": "14:36:57.000", "Lon": 124.27380469972222, "Lat": 8.0227202997222236, "Altitude": null, "North": null, "Azimuth": null, "Camera Mak": "samsung", "Camera Mod": "SM-G975F", "Title": null, "Comment": null, "Path": "C:\/Users\/Jake Rom Cadag\/Documents\/Academic\/GIS\/Mastery\/Import_Photos\\20200301_143657.jpg", "RelPath": ".\/Import_Photos\/20200301_143657.jpg", "Timestamp": "2020\/03\/01 14:36:57.000" }, "geometry": { "type": "Point", "coordinates": [ 124.27380469972222, 8.022720299722224 ] } },
+{ "type": "Feature", "properties": { "ID": null, "Name": "Talayan", "Date": null, "Time": null, "Lon": null, "Lat": null, "Altitude": null, "North": null, "Azimuth": null, "Camera Mak": null, "Camera Mod": null, "Title": null, "Comment": null, "Path": "C:\/Users\/Jake Rom Cadag\/Documents\/Academic\/GIS\/Mastery\/Import_Photos\\20200201_151835.jpg", "RelPath": ".\/Import_Photos\/20200201_151835.jpg", "Timestamp": null }, "geometry": { "type": "Point", "coordinates": [ 121.013084335246376, 14.635306078164444 ] } }
+]
+}
